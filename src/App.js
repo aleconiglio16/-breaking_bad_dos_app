e2628145f6db732d86a6620d259fe9bd08f11022
aleconiglio16 <alejandroconiglio16@gmail.com>
2022-05-01 04:20:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Header from './componentes/ui/Header';
+import CharacterGrid from "./componentes/characters/CharacterGrid";
+import Search from "./componentes/ui/Search";
 
-function App() {
+
+const App = () => {
+
+  const [ items, setItems ] = useState([]);
+  const [ isLoading, setLoading ] = useState(true);
+  const [ query, setQuery] = useState(""); 
+
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+
+      setItems(result.data)
+      setLoading(false);
+    }
+
+
+    fetchItems();
+  }, [query])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid isLoading = { isLoading } items= { items } />
+
     </div>
   );
 }
---
+++ src/componentes/ui/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../API BB/logo.png"
+
+
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={ logo } alt="Breaking Bad" />
+        </header>
+    )
+}
+
+export default Header
